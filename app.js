b6711e3caa8bce232401342ea7b275648c702a6c
Cheyna Humphries <humphries.c92@gmail.com>
2018-06-08 20:37:38 -0700

    updated changes to question 3 still working

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
+
 function sum(a, b) { //eslint-disable-line
   var theSum = a + b;
   var theMessage = 'The sum of ' + a + ' and ' + b + ' is ' + theSum + '.';
@@ -54,11 +55,23 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+  var totalSum = sum(a, b)[0];
+  console.log(totalSum + ' is the answer to question 1');
 
+  var sumOfThreeNum = (totalSum ^ c);
+  console.log(sumOfThreeNum + ' this is the sum of all three numbers');
+  var totalSumMessage = (a + ' and ' + b + ' and ' + c + ' sum to ' + sumOfThreeNum);
+
+  var totalProduct = multiply(a, b)[0];
+  var productThreeNum = (totalProduct ^ c);
+
+  return[totalSum, sumOfThreeNum, totalProduct, productThreeNum];
 }
 
+
+
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4, 7, 5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
